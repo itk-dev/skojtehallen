6d6f5317676fc3b2e6666883949dfcc3352e5201
Jesper Kristensen <cableman0408@gmail.com>
2010-02-23 17:56:13 +0100

    removed xml view

--- sites/all/themes/skojtehallen/templates/page/page-xml.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php
-drupal_set_header('Content-Type: text/xml; charset=utf-8');
-print "<?xml version=\"1.0\" encoding=\"utf-8\"?>"; ?>
-<data delay="4000">
-  <frontImage>/sites/all/modules/custom/fading_banner/img/front.png</frontImage>
-  <?php print $content; ?>
-</data>
