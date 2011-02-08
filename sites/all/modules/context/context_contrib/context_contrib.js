9e0f8d4165bba240aadbf1eaa703b556be1328de
Jesper Kristensen <jeskr@aarhus.dk>
2011-02-08 14:22:12 +0100

    Updated core and modules

--- sites/all/modules/context/context_contrib/context_contrib.js
+++ sites/all/modules/context/context_contrib/context_contrib.js
@@ -1,10 +1,10 @@
-// $Id: context_contrib.js,v 1.1.2.1 2010/04/21 01:03:22 darthsteven Exp $
+// $Id: context_contrib.js,v 1.1.2.2 2010/12/21 09:50:22 darthsteven Exp $
 
 /**
  * This behavior looks at the views checkboxes that are being selected and if all the displays on a single view are ticked, then tick that too. Likewise if they are all ticked, and one is then unticked, untick the all checkbox. Toggling the parent checkbox toggles the children too.
  */
 Drupal.behaviors.context_contrib_views_checkboxes = function(context) {
-  $('#widget-views :checkbox').change(function() {
+  $('#widget-views').find('input:checkbox').change(function() {
     var element = $(this);
     var val = element.val();
     // Children
